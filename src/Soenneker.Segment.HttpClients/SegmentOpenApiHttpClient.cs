40552c89c48cec0d217258ba98a64f72908e38a3
soenneker <jake@soenneker.com>
2026-08-29 14:51:07 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Segment.HttpClients/SegmentOpenApiHttpClient.cs
+++ src/Soenneker.Segment.HttpClients/SegmentOpenApiHttpClient.cs
@@ -11,7 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Segment.HttpClients;
 
-///<inheritdoc cref="ISegmentOpenApiHttpClient"/>
+/// <inheritdoc cref="ISegmentOpenApiHttpClient"/>
 public sealed class SegmentOpenApiHttpClient : ISegmentOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
