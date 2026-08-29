cd53564939aba6b19807fb0eb1916fad2cd1bfcf
soenneker <jake@soenneker.com>
2026-08-29 14:17:18 -0500

    Improve public API XML documentation

--- src/Soenneker.Segment.HttpClients/Registrars/SegmentOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Segment.HttpClients/Registrars/SegmentOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class SegmentOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="SegmentOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddSegmentOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class SegmentOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="SegmentOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddSegmentOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
